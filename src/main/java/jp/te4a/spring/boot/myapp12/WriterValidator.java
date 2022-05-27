6a522f3439aedfdf0dd7d69c272696236e3c7dd7
190738 <190738@e0601-14.JC-21.local>
2022-05-27 14:48:52 +0900

    課題12終わり

---
+++ src/main/java/jp/te4a/spring/boot/myapp12/WriterValidator.java
@@ -0,0 +1,15 @@
+package jp.te4a.spring.boot.myapp12;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+public class WriterValidator implements ConstraintValidator<Writer,String>{
+	String ok;
+	@Override
+	public void initialize(Writer nv){ ok =  nv.ok(); }
+	@Override
+	public boolean isValid(String in,ConstraintValidatorContext cxt){
+		if(in == null){
+			return false;
+		}
+		System.out.println(in.equals(ok));
+		return in.equals(ok);
+}}
